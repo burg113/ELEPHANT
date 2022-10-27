cffa800ccc7dbd8b0877599376da589e4d9f90e8
sn00py1310 <61204194+sn00py1310@users.noreply.github.com>
2022-10-27 23:35:11 +0200

    Multiple updates Updated whitelisting, updated logging, added in calendar errors

--- src/main.py
+++ src/main.py
@@ -6,10 +6,23 @@ from werkzeug.exceptions import HTTPException
 import logging
 import logging.handlers
 import requests
-from urllib.parse import urlparse
+from hyperlink import parse
+from icalendar import Calendar, Event
+from datetime import datetime, timedelta
 
-LOG_FILE = ""
+APP_NAME = "ELEPHANT"
+APP_VERSION = "0.0.1"
+APP_URL = "https://github.com/burg113/ELEPHANT"
+APP_DEBUG = True
+LOG_FILE = "logs.log"
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+CONFIG_ORIGIN_WHITELIST = {
+    "campus.kit.edu": {
+        "scheme": ["http", "https"],
+        "path": ["/sp/webcal/"],
+    }
+}
 
 # BEGIN Log configs
 log = logging.getLogger(__name__)
@@ -18,7 +31,6 @@ log_formatter = logging.Formatter(LOG_FORMAT)
 if LOG_FILE:
     log_file_handler = logging.handlers.RotatingFileHandler(
         f"{LOG_FILE}", maxBytes=5*1024*1024, backupCount=3)
-    # logging.FileHandler(f"{LOG_FILE}")
     log_file_handler.setFormatter(log_formatter)
     log.addHandler(log_file_handler)
 
@@ -33,7 +45,8 @@ app = Flask(__name__)
 @app.errorhandler(HTTPException)
 def page_not_found(e):
     log.warning(e)
-    return e.name, e.code
+    resp = cal_errors(e)
+    return resp
 
 # @app.errorhandler(HTTPException)
 # def handle_http_exception(e: HTTPException) -> ResponseReturnValue:
@@ -62,15 +75,19 @@ def page_not_found(e):
 
 
 # BEGIN App routes
-@app.route("/v1")
+@app.route("/v0")
 def api_v1() -> ResponseReturnValue:
+
+    all_headers = str(request.headers).replace("\n", "")
+    log.debug(f"Request from {all_headers}")
+
     url = request.args.get("cal")
 
     if not url:
-        abort(404, "Error: No Calendar URL found")
+        abort(404, "No Calendar URL found")
 
-    if not check_cal_url(url):
-        abort(403, "Error: Not an allowed Calendar origin")
+    if not check_cal_url(url, CONFIG_ORIGIN_WHITELIST):
+        abort(403, "Not an allowed Calendar origin")
 
     cal = fetch_cal(url)
 
@@ -82,31 +99,29 @@ def api_v1() -> ResponseReturnValue:
 
 
 # BEGIN Helper functions
-def check_cal_url(org_url: str) -> bool:
+def check_cal_url(org_url: str, whitelist: dict) -> bool:
 
-    sources_whitelist = {
-        "campus.kit.edu": {
-            "scheme": ["http", "https", "webcals"],
-            "path": ["/sp/webcal/"],
-        }
-    }
-
-    url = urlparse(org_url)
-    if url.hostname not in sources_whitelist:
+    url = parse(org_url)
+    print(url)
+    if url.host not in whitelist:
         log.debug(f"Hostname not whitelisted {url=}")
+        abort(403, f"Hostname not whitelisted, {url.host}")
         return False
 
-    source_options = sources_whitelist[url.hostname]
+    source_options = whitelist[url.host]
     if source_options["scheme"] and url.scheme not in source_options["scheme"]:
         log.debug(f"Scheme not whitelisted {url=}")
+        abort(403, f"Scheme not whitelisted, {url.scheme}")
         return False
 
+    url_path = "/" + "/".join(url.path)[:-1]
     if source_options["path"]:
         for path in source_options["path"]:
-            if url.path.startswith(path):
+            if url_path.startswith(path):
                 break
         else:
             log.debug(f"Path not whitelisted {url=}")
+            abort(403, f"Path not whitelisted, {url_path}")
             return False
 
     log.debug(f"URL passed all checks {url=}")
@@ -115,13 +130,19 @@ def check_cal_url(org_url: str) -> bool:
 
 def fetch_cal(url: str) -> str:
     try:
-        a = requests.get(url)
+        r = requests.get(url, headers={"User-Agent": f"{APP_NAME}-Importer"})
         log.info(f"Fetched {url=}")
-    except Exception as e:
+        if not r.ok:
+            log.debug(f"Couldn't fetch {url}")
+            abort(
+                500, f"Couldn't fetch the calendar, HTTP status code {r.status_code}")
+    except requests.exceptions.RequestException as e:
         log.error(f"{e}")
-        abort(500)
+        abort(500, "There has been an error while fetching the Calendar")
 
-    return a.text
+    r.encoding = r.apparent_encoding
+    return r.text
+
 
 # TODO: Extract replacements from URI, standard needed
 def get_replacement_list(request: Request) -> list[list[str]]:
@@ -131,40 +152,70 @@ def get_replacement_list(request: Request) -> list[list[str]]:
 def cal_handler(cal: str,
                 replacements: list[list[str]] = list(list())
                 ) -> str:
-    # replacements = [[
-    #     "NAME:KIT-Termine", "NAME:Proxy-KIT-Termine"
-    # ]]
+    # replacements = [
+    #     ["Übungen zu 0133200 (Ü)",
+    #      "(Ü) Höhere Mathematik I (Analysis) für die Fachrichtung Informatik - Übungen"],
+    #     ["Übungen zu 0133000 (Ü)",
+    #      "(Ü) Lineare Algebra I für die Fachrichtung Informatik - Übungen"]
+    # ]
+    # Inject Custom Header
+    try:
+        ical = Calendar.from_ical(cal)
+        ical["NAME"] = f"{APP_NAME} {ical['NAME']}"
+        ical["URL"] = APP_URL
+        ical["REFRESH-INTERVAl;VALUE=DURATION"] = "PT30M"  # Update every 30 min
+        cal = str(ical.to_ical(), "utf-8")
+    except ValueError as e:
+        log.debug(f"Could not parse {cal}")
+        abort(500, "There has been an error with parsing the Calender")
 
     for i in replacements:
-        cal = cal.replace(i[0], i[1])
+        if len(i) == 2:
+            cal = cal.replace(i[0], i[1])
+        else:
+            log.warn("Replacements list length not valid")
 
     return cal
 
 
-def add_http_headers(resp: Response, filename: str = "Proxy-Calendar") -> Response:
-    # resp.headers["Content-Type"] = "text/plain"  # For debugging
+def add_http_headers(resp: Response, filename: str = f"{APP_NAME}-Proxy-Calendar") -> Response:
+    resp.headers["Content-Type"] = "text/plain; Charset=utf-8"  # For debugging
 
-    resp.headers["Content-Type"] = "text/calendar"
-    resp.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    # resp.headers["Content-Type"] = "text/calendar; Charset=utf-8"
+    # resp.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    return resp
+
+
+def cal_errors(e: HTTPException) -> Response:
+    ev = Event()
+    ev.add("DTSTART", datetime.today().date())
+    ev.add("DTEND", datetime.today().date() + timedelta(days=365))
+    ev.add("SUMMARY", f"{APP_NAME}, {e.code} - {e.name}")
+    ev.add("DESCRIPTION", e.description)
+
+    cal = Calendar()
+    cal.add("NAME", f"{APP_NAME} Error")
+    cal.add_component(component=ev)
+
+    resp = make_response(
+        cal.to_ical(), e.code
+    )
+    resp = add_http_headers(resp)
     return resp
 
 
 def main() -> None:
-    app.run(debug=True)
-
-
-def tests() -> None:
-    log.debug(check_cal_url("https://campus.kit.edu/sp/webcal/IU1b24BcgM"))
-    log.debug(check_cal_url("http://campus.kit.edu/sp/webcal/IU1b24BcgM"))
-    log.debug(check_cal_url(
-        "webcals://campus.kit.edu/sp/webcal/IU1b24BcgM?tguid=0xB7532209C5264C53A99D9128A9F9A321"))
-    log.debug(check_cal_url("https://google.com"))
-
-    # Eigenen URL parser machen? Auf keinen fall https://stackoverflow.com/a/10988764
-    # check_cal_url("http://netloc/path;someparam=some;otherparam=other?query1=val1&query2=val2#frag")
-    # check_cal_url("http://some.page.pl/nothing.py;someparam=some;otherparam=other?query1=val1&query2=val2#frag")
+    print(
+"""
+ _______  __       _______ .______    __    __       ___      .__   __. .___________.
+|   ____||  |     |   ____||   _  \  |  |  |  |     /   \     |  \ |  | |           |
+|  |__   |  |     |  |__   |  |_)  | |  |__|  |    /  ^  \    |   \|  | `---|  |----`
+|   __|  |  |     |   __|  |   ___/  |   __   |   /  /_\  \   |  . `  |     |  |     
+|  |____ |  `----.|  |____ |  |      |  |  |  |  /  _____  \  |  |\   |     |  |     
+|_______||_______||_______|| _|      |__|  |__| /__/     \__\ |__| \__|     |__|     
+""")
+    app.run(debug=APP_DEBUG)
 
 
 if __name__ == "__main__":
     main()
-    # tests()
